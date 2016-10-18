641c404f14a2a34649ce5a3a0712ab3b8eede197
Tim Konieczny <timkonieczny@gmail.com>
2016-10-18 18:13:17 +0200

    Enabled tab indenting in textarea

--- helpers.js
+++ helpers.js
@@ -44,3 +44,17 @@ var showNotification = function(text, icon){
     notificationText.innerHTML = text;
     notificationIcon.className = "fa " + icon;
 };
+
+// Enables \t indenting in textareas
+var textareas = document.getElementsByTagName('textarea');
+var count = textareas.length;
+for(var i=0;i<count;i++){
+    textareas[i].onkeydown = function(e){
+        if(e.keyCode==9 || e.which==9){
+            e.preventDefault();
+            var s = this.selectionStart;
+            this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
+            this.selectionEnd = s+1;
+        }
+    }
+}
