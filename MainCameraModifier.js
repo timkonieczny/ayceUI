d099e3964ae9c2061c701e7d0daa1cdb3a3384c8
Tim Konieczny <timkonieczny@gmail.com>
2016-11-15 18:04:17 +0100

    Added direct manipulation of main camera orientation

--- MainCameraModifier.js
+++ MainCameraModifier.js
@@ -6,12 +6,21 @@ MainCameraModifier = function (canvas) {
         isMiddleDown: false,
         position: new Ayce.Vector2(0, 0),
         lastPosition: new Ayce.Vector2(0, 0),
-        isInitialized: false
+        isInitialized: false,
+        movement: new Ayce.Vector2(0, 0)
     };
 
     var moveSpeed = 0.01;
+    var rotSpeed = 0.2;
     var position = new Ayce.Vector3();
     var rotation = new Ayce.Quaternion();
+    var rotX = 0;
+    var rotY = 0;
+    var xAxis = new Ayce.Quaternion();
+    var yAxis = new Ayce.Quaternion();
+    var trivialX = new Ayce.Vector3(1,0,0);
+    var trivialY = new Ayce.Vector3(0,1,0);
+    var rotatedTranslation = new Ayce.Vector3(0, 0, 0);
 
     canvas.addEventListener("contextmenu", function (e) {
         e.preventDefault();
@@ -53,20 +62,46 @@ MainCameraModifier = function (canvas) {
     });
 
     canvas.addEventListener("mousemove", function (e) {
-        if(mouse.isLeftDown){
+        if(mouse.isLeftDown||mouse.isMiddleDown||mouse.isRightDown){
             mouse.position.x = e.x;
             mouse.position.y = e.y;
         }
     });
 
     this.update = function(orientation){
-        if(mouse.isLeftDown){
+        if(mouse.isLeftDown||mouse.isRightDown||mouse.isMiddleDown){
             if(mouse.isInitialized){
-                position.add(
-                    (-mouse.position.x+mouse.lastPosition.x)*moveSpeed,
-                    (mouse.position.y-mouse.lastPosition.y)*moveSpeed,
-                    0
-                );
+                mouse.movement.x = (-mouse.position.x + mouse.lastPosition.x);
+                mouse.movement.y = (mouse.position.y - mouse.lastPosition.y);
+                if(mouse.isLeftDown) {
+                    rotatedTranslation.x = mouse.movement.x;
+                    rotatedTranslation.y = mouse.movement.y;
+                    rotatedTranslation.z = 0;
+                    rotatedTranslation = orientation.getRotatedPoint(rotatedTranslation);
+                    position.add(
+                        rotatedTranslation.x * moveSpeed,
+                        rotatedTranslation.y * moveSpeed,
+                        rotatedTranslation.z * moveSpeed
+                    );
+                }else if(mouse.isRightDown){
+                    rotX += mouse.movement.x;       // mouse movement in x direction
+                    rotY += -mouse.movement.y;      // mouse movement in y direction
+                    //Cap rotation around x axis
+                    if (rotY * rotSpeed > 90)
+                        rotY = 90 / rotSpeed;
+                    else if (rotY * rotSpeed < -90)
+                        rotY = -90 / rotSpeed;
+
+                    //Rotation around y axis from x mouse movment
+                    yAxis.reset();
+                    yAxis.fromAxisAngle(trivialY, rotX * rotSpeed * Math.PI / 180);
+                    //Rotation around x axis from y mouse movment
+                    xAxis.reset();
+                    xAxis.fromAxisAngle(trivialX, rotY * rotSpeed * Math.PI / 180);
+
+                    xAxis.multiply(xAxis, yAxis);
+                    rotation = xAxis;
+                }
             }else{
                 mouse.isInitialized = true;
             }
