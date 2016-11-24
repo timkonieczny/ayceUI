a43c71fac8795ea34b3d9ad070f37e4421d00b65
Tim Konieczny <timkonieczny@gmail.com>
2016-11-24 23:15:14 +0100

    Simplified grid rendering

--- BasePlane.js
+++ BasePlane.js
@@ -20,29 +20,59 @@ BasePlane.prototype.getO3D = function () {
     var startX = -width/2;
     var startZ = -height/2;
 
-    o3D.indices = [];
     o3D.vertices = [];
+    o3D.indices = [];
+    o3D.colors = [];
 
-    for(var i = 0; i < subX; i++){
-        for(var j = 0; j < subZ; j++) {
-            o3D.vertices.push(
-                startX+i*offsetX, 0, startZ+j*offsetZ
+    for(var i = 0; i < subZ; i++){
+        o3D.vertices.push(
+            startX, 0, startZ+i*offsetZ,
+            -startX, 0, startZ+i*offsetZ
+        );
+
+        if(i == Math.floor(subZ/2)){
+            o3D.colors.push(
+                1.0, 0.5, 0.5, 1.0,
+                1.0, 0.5, 0.5, 1.0
+            );
+        }else {
+            o3D.colors.push(
+                0.5, 0.5, 0.5, 1.0,
+                0.5, 0.5, 0.5, 1.0
             );
         }
     }
-    for(i = 0; i < subZ; i++){
-        for(j = 0; j < subX; j++) {
-            o3D.indices.push(i + j*subZ);
-            if(j>0 && j < subX-1)
-                o3D.indices.push(i + j*subZ);
+
+    for(i = 0; i < subX; i++){
+        o3D.vertices.push(
+            startX+i*offsetX, 0, startZ,
+            startX+i*offsetX, 0, -startZ
+        );
+
+        if(i == Math.floor(subX/2)){
+            o3D.colors.push(
+                0.5, 0.5, 1.0, 1.0,
+                0.5, 0.5, 1.0, 1.0
+            );
+        }else {
+            o3D.colors.push(
+                0.5, 0.5, 0.5, 1.0,
+                0.5, 0.5, 0.5, 1.0
+            );
         }
     }
-    for(i = 0; i < subX; i++){
-        for(j = 0; j < subZ; j++){
-            o3D.indices.push(i*subZ+j);
-            if(j>0 && j < subZ-1)
-                o3D.indices.push(i*subZ+j);
-        }
+
+    o3D.vertices.push(
+        0, height/2, 0,
+        0, -height/2, 0
+    );
+    o3D.colors.push(
+        0.5, 1.0, 0.5, 1.0,
+        0.5, 1.0, 0.5, 1.0
+    );
+
+    for(i = 0; i < o3D.vertices.length/3; i++){
+        o3D.indices.push(i);
     }
 
     o3D.position = this.position;
